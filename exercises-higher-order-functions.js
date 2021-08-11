d659355aa0f5d7dc97a5367fab2ff8dad9f955b3
Welber33 <jwwork21@gmail.com>
2021-08-11 11:06:52 -0300

    Taking out the comma of the array

--- exercises-higher-order-functions.js
+++ exercises-higher-order-functions.js
@@ -49,7 +49,7 @@ function every(array, test) {
     return true;
 }
 
-const arrayElements = [1, 2, 3, 4, 5,];
+const arrayElements = [1, 2, 3, 4, 5];
 console.log(every(arrayElements, n => n < 5));
 
 
